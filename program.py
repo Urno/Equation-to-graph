0cec5ceb9407d5ccd2c8e0fb2ae414b781b958f9
Urno <Urno@gmail.com>
2017-10-07 18:02:47 +0900

    First

--- program.py
+++ program.py
@@ -1,6 +1,18 @@
-#프로그램
-#1. 식 입력하기
-#2. 변수와 상수 설정
+#함수 부분
+def varchange(list):
+    for k in list:
+        k=int(input("변수 "+k+"의 값을 입력하세요. : "))
+def conchange(list):
+    for k in list:
+        k=int(input("상수 "+k+"의 값을 입력하세요. : "))
+#1. 변수와 상수 입력하기
+var=list(input("변수를 입력하세요. : "))
+con=list(input("상수를 입력하세요. : "))
+#2. 식 입력하기
+f=input("식을 입력하세요. : ")
+revise(f)
+varchange(var)
+conchange(con)
 #3. 프로그램의 활용 설정(근삿값 찾기, 그래프 그리기 등)
 #4. 해석형 프로그램
 #5. 결과 출력 및 재가동
