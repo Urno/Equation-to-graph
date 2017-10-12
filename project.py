25d8eb7415a159ede0915533548de884c6197647
Urno <Urno@gmail.com>
2017-10-12 10:10:45 +0900

    sixth

---
+++ project.py
@@ -0,0 +1,14 @@
+import input
+import process
+import output
+
+#main
+process.value(x)
+process.constant(x)
+
+process.Numpy(x)
+output.result(f)
+if n==0:
+    output.graph(f)
+elif n==1:
+    output.approximation(f)
