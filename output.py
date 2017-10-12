08dd81109fe9856cbdecd1b1829e000ed7293d80
Urno <Urno@gmail.com>
2017-10-12 10:06:29 +0900

    fifth

---
+++ output.py
@@ -0,0 +1,7 @@
+#5. 결과 출력
+def result(x):
+n=input("출력할 내용을 설정해주세요.")
+def graph(x):
+    print("그래프 출력 함수")
+def approximation(x):
+    print("근삿값 출력 함수")
---
+++ process.py
@@ -0,0 +1,23 @@
+import input
+import output
+def revise(x):
+    print("함수의 각 기능 부분을 분류")
+
+#main
+input.varchange(list)
+input.conchange(list)
+revise(f)
+
+#3. 프로그램의 활용 설정
+def value(x):
+    print("미지수의 값을 변화시키는 함수")
+def constant(x):
+    print("상수의 값을 변화시키는 함수")
+
+#4. 수치 해석 프로그램 작동
+print("Numpy")
+output.result(f)
+if n==0:
+    output.graph(f)
+elif n==1:
+    output.approximation(f)
